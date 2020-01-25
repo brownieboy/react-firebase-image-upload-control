c1285910cdb87e16219578c279c356c75a30f89e
Michael Brown <brownieboy@gmail.com>
2020-01-25 12:47:50 +1100

    Webpack BannerPlugin to add eslint comment

--- component/webpack.config.js
+++ component/webpack.config.js
@@ -1,17 +1,32 @@
 const path = require("path");
-const TARGET = process.env.npm_lifecycle_event;
+const webpack = require("webpack");
 
-const OUTPUTPATH = path.resolve(__dirname, "..", "examples", "src", "component");
-console.log("TCL: OUTPUTPATH", OUTPUTPATH);
+const TARGET = process.env.npm_lifecycle_event;
+const DEVMODE = TARGET === "buildforexamples";
+
+let plugins = [];
+if (DEVMODE) {
+  // Necessary because otherwise Create React App will lint the file and fail to
+  // build due to "define is undefined" error
+  plugins = [
+    new webpack.BannerPlugin({
+      banner: "/* eslint-disable */",
+      raw: true
+    })
+  ];
+}
 
 module.exports = {
   entry: "./src/index.js",
+  mode: DEVMODE ? "development" : "production",
   output: {
     path:
       TARGET === "buildforexamples"
-        ? path.resolve(__dirname, "..", "examples", "src", "component")
+        ? path.resolve(__dirname, "..", "rfiu-examples", "src", "component")
         : path.resolve(__dirname, "../dist"),
-    filename: "index.js"
+    filename: "index.js",
+    library: "rfiu",
+    libraryTarget: "umd"
   },
   module: {
     rules: [
@@ -26,5 +41,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins
 };
--- rfiu-examples/src/App.js
+++ rfiu-examples/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import MyModule from "./component/index.js";
 
+// console.log(whatever);
+
 function App() {
   return (
     <div className="App">
