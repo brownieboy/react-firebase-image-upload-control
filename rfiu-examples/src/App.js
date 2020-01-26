81027b6ae257860417bd14ab2ed9f45b24def278
Michael Brown <brownieboy@gmail.com>
2020-01-26 13:47:41 +1100

    Email password autentication working

--- rfiu-examples/src/App.js
+++ rfiu-examples/src/App.js
@@ -1,16 +1,47 @@
 import React from "react";
 import "./App.css";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import withFirebaseAuth from "react-with-firebase-auth";
+
 import MyModule from "./component/index.js";
+import UserForm from "./UserForm";
+
+// You must supply this!
+import firebaseConfigObj from "./firebaseconfig/firebase-config.json";
+
+const firebaseApp = firebase.initializeApp(firebaseConfigObj);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
 
 // console.log(whatever);
 
-function App() {
+const App = ({ loading, signInWithEmailAndPassword, signOut, user }) => {
   return (
     <div className="App">
       <h1>My Module Test</h1>
       <MyModule />
+      {user ? <h1>Hello, {user.displayName}</h1> : <h1>Log in</h1>}
+
+      {user ? (
+        <button onClick={signOut}>Sign out</button>
+      ) : (
+        <>
+          <h3>sign in</h3>
+          <UserForm onSubmit={signInWithEmailAndPassword} />
+        </>
+      )}
+
+      {loading && <h2>Loading..</h2>}
     </div>
   );
-}
+};
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);
---
+++ rfiu-examples/src/UserForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+import compose from "recompose/compose";
+import withState from "recompose/withState";
+
+const Field = ({ children }) => (
+  <React.Fragment>
+    {children}
+    <br />
+  </React.Fragment>
+);
+
+const Input = ({ value, onChange, ...props }) => (
+  <input
+    {...props}
+    value={value}
+    onChange={event => onChange(event.target.value)}
+  />
+);
+
+const SubmitButton = props => <button {...props}>submit</button>;
+
+const UserForm = ({ setEmail, setPassword, email, password, onSubmit }) => (
+  <React.Fragment>
+    <Field>
+      email: <Input value={email} onChange={setEmail} />
+    </Field>
+    <Field>
+      password:{" "}
+      <Input value={password} onChange={setPassword} type="password" />
+    </Field>
+    <SubmitButton onClick={() => onSubmit(email, password)} />
+  </React.Fragment>
+);
+
+const withUserFormState = compose(
+  withState("email", "setEmail"),
+  withState("password", "setPassword")
+);
+
+export default withUserFormState(UserForm);
