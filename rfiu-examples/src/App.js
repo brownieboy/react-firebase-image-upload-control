756d09fcf6e5126b02de69260d91d878ab04f9b2
Michael Brown <brownieboy@gmail.com>
2020-01-27 12:25:01 +1100

    Adding login type radio

--- rfiu-examples/src/App.js
+++ rfiu-examples/src/App.js
@@ -21,22 +21,44 @@ const providers = {
 
 // console.log(whatever);
 
-const App = ({ loading, signInWithEmailAndPassword, signOut, user }) => {
-  console.log("TCL: App -> loading", loading);
-  const [signInResult, setSignInResult] = useState({});
+const LOGIN_TYPES = [
+  { id: "email", label: "Email and password" },
+  { id: "google", label: "Google ID" }
+];
 
-  const handleSignInWithEmailAndPassword = async (email, password ) => {
+const App = ({ loading, signInWithEmailAndPassword, signOut, user }) => {
+  const [signInResult, setSignInResult] = useState({});
+  const [loginType, setLoginType] = useState("email");
+
+  const handleSignInWithEmailAndPassword = async (email, password) => {
     const newSignInResult = await signInWithEmailAndPassword(email, password);
     setSignInResult(newSignInResult);
     console.log("TCL: App -> newSignInResult", newSignInResult);
   };
+
+  const handleChangeLoginType = e => setLoginType(e.target.value);
 
   return (
     <div className="App">
       <h1>My Module Test</h1>
       {/* <MyModule /> */}
       {user ? <h1>Hello, {user.displayName}</h1> : <h1>Log in</h1>}
-
+      <p>You need to log into Firebase first. Please choose your login type</p>
+      {LOGIN_TYPES.map(type => (
+        <>
+          <label>
+            <input
+              type="radio"
+              value={type.id}
+              onChange={handleChangeLoginType}
+              name="logintype"
+              checked={loginType === type.id}
+            />
+            {type.label}
+          </label>
+          <br />
+        </>
+      ))}
       {user ? (
         <button onClick={signOut}>Sign out</button>
       ) : (
@@ -47,7 +69,7 @@ const App = ({ loading, signInWithEmailAndPassword, signOut, user }) => {
       )}
 
       {/* {loading && <h2>Loading..</h2>} */}
-      <div style={{marginTop: 20}}>
+      <div style={{ marginTop: 20 }}>
         <b>Last sign-in message:</b>
         <div style={{ fontSize: 11 }}>
           {_isEmpty(signInResult) ? "No logins yet.." : signInResult.message}
