47d97d7282fb44fd8ef7bc14529f343097fb2090
Michael Brown <brownieboy@gmail.com>
2022-09-30 17:02:03 +1000

    chore: building wp5 but node_modules too

--- component/webpack.config.js
+++ component/webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-env node */
 const path = require("path");
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -7,11 +9,39 @@ module.exports = {
   entry: "./src/index.js",
   mode: DEVMODE ? "development" : "production",
   devtool: DEVMODE ? "eval-source-map" : "cheap-source-map",
+  target: "node",
+  externals: {
+    firebase: "firebase",
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  externalsPresets: {
+    node: true, // in order to ignore built-in modules like path, fs, etc.
+  },
   output: {
     path:
       TARGET === "start"
         ? path.resolve(__dirname, "..", "demo", "src", "package")
         : path.resolve(__dirname, "../dist"),
     filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "./src"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
