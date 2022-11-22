686398b035d1b14c358ed2ece5583629ed23e1b8
Michael Brown <brownieboy@gmail.com>
2022-11-22 17:16:50 +1100

    chore: add ImageDrop test

--- component/tests/ImageDrop.test.tsx
+++ component/tests/ImageDrop.test.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import {render} from "@testing-library/react";
+import ImageDrop from "../src/ImageDrop";
 
 describe("<ImageDrop />", () => {
-  it("should show a div", () => {
-    const component = <div>Facebook</div>;
+  it("should show a component", () => {
+    const component = <ImageDrop />;
     const {container} = render(component);
     expect(container.children.length).toBe(1);
   });
 
   it("should match the snapshot correctly", () => {
-    const component = <div>Facebook</div>;
+    const component = <ImageDrop />;
     const tree = renderer.create(component).toJSON();
     expect(tree).toMatchSnapshot();
   });
